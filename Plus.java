4a60dbb2c0367ccddd77c40ff7abe3ba6a161c91
1221303353 <1221303353@student.mmu.edu.my>
2026-02-27 22:32:11 +0800

    Initial Commit

---
+++ Plus.java
@@ -0,0 +1,46 @@
+// Model class to represent the Plus piece
+// Parthasarathy A/L Raman (1191102837)
+public class Plus extends Piece {
+
+    public Plus(String color) {
+        super(color);
+    }
+
+    @Override
+    public boolean isValidMove(int currentRow, int currentCol, int targetRow, int targetCol, Board board) {
+
+        boolean sameX = (currentCol == targetCol);
+        boolean sameY = (currentRow == targetRow);
+
+        if (sameX) {
+            for (int i = 1; i < Math.abs(targetRow - currentRow); i++) {
+                if (currentRow < targetRow && board.getBoardPiece()[currentRow + i][currentCol] != null) {
+                    return false;
+                } else if (currentRow > targetRow && board.getBoardPiece()[currentRow - i][currentCol] != null) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        if (sameY) {
+            for (int i = 1; i < Math.abs(targetCol - currentCol); i++) {
+                if (currentCol < targetCol && board.getBoardPiece()[currentRow][currentCol + i] != null) {
+                    return false;
+                } else if (currentCol > targetCol && board.getBoardPiece()[currentRow][currentCol - i] != null) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // Add this method to get the icon name
+    public String getIconName() {
+        return pieceColor().equals("Yellow") ? "YPlus" : "BPlus";
+    }
+}
